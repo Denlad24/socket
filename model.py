33e0b6545efe4ebbdd816084cc2801a8e82616a7
Denlad24 <55156774+Denlad24@users.noreply.github.com>
2021-04-29 15:39:05 +0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,19 @@
+notes = {}
+
+
+def get_note(id):
+    if id in notes:
+        return notes[id]
+    return "NO NOTE FOUND"
+
+
+def save_note(id, note):
+    notes[id] = note
+
+
+def get_notes():
+    return notes
+
+
+def delete_note(id):
+    notes.pop(id)
