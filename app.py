eae68acf475be31ac8d5819e9293338b69c3614e
Denlad24 <55156774+Denlad24@users.noreply.github.com>
2021-04-29 13:27:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from threading import Lock
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import model
@@ -9,20 +8,6 @@ async_mode = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
-thread = None
-thread_lock = Lock()
-
-
-# def background_thread():
-#     """Example of how to send server generated events to clients."""
-#     count = 0
-#     while True:
-#         socketio.sleep(10)
-#         count += 1
-#         socketio.emit('my_response',
-#                       {'data': 'Server generated event', 'count': count},
-#                       namespace='/test')
-
 
 @app.route('/')
 def index():
