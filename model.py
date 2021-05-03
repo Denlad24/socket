8cf6c92a0ca09e86ac7c655cd1f81a536332fec8
Denlad24 <55156774+Denlad24@users.noreply.github.com>
2021-05-03 17:17:05 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,20 +1,15 @@
-notes = {}
-
+notes = {"1", "asdada"}
 
 def get_note(id):
     if id in notes:
         return notes[id]
     return notes
 
-
 def save_note(id, note):
     notes[id] = note
-    return notes
-
 
 def get_notes():
     return notes
-
 
 def delete_note(id):
     notes.pop(id)
